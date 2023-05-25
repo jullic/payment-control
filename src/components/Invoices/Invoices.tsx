26c71082aa3f4e7bdcf27034e882a61b49dfad02
Aleksandr Lvov <iJullic@yandex.ru>
2023-05-25 21:59:16 +0300

    update message

--- src/components/Invoices/Invoices.tsx
+++ src/components/Invoices/Invoices.tsx
@@ -33,7 +33,7 @@ export const Invoices: FC<IInvoicesProps> = ({
 	return (
 		<div className={classNames(styles.root, className)} {...props}>
 			<div className={classNames(styles.header)}>
-				<h2>{isToday ? 'Сегодня' : date}</h2>
+				<h2>{isToday ? `Сегодня (${date})` : date}</h2>
 				<Button onClick={onCopyHandler}>Скопировать</Button>
 			</div>
 			<div className={classNames(styles.invoices)}>
